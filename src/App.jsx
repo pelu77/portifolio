a1e0f4dc0f7226e6a0ef1500d515ea9847393be3
Pedro Lucas <pedrolrc27@gmail.com>
2026-09-25 19:27:16 -0300

    Corrige import do Navbar (case-sensitive)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import Apresentacao from './pages/apresentacao';
 import Projetos from './pages/projetos';
 import Tecnologias from './pages/tecnologias';
